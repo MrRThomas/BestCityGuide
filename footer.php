229bdf56e50af56e3a134b8d1630c4a9c32a5240
Robert Thomas <FireSqwall@Gmail.com>
2018-11-15 11:51:46 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
+		</div><!--/.container-two-->
+		
+	</div><!--/.wrap-->
+
 <footer class="main-footer">
 	 <span>&copy; 2015-<?php echo date('Y'); ?> Residents of The Best City Ever.</span>
 	</footer>
