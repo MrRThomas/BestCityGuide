a60804b70da9b7f7298172a42c31e7994a95bde7
Robert Thomas <FireSqwall@Gmail.com>
2018-11-15 11:53:19 -0500

    Adding the proper navigaton

--- header.php
+++ header.php
@@ -13,11 +13,26 @@
 		<header class="main-header">
 			<div class="container">
 				<h1 class="name"><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
+				<?php
+				
+				    $defaults = array(
+				      'container' => false,
+				      'theme_location' => 'primary-menu',
+				      'menu_class' => 'main-nav'
+				    );
+				    
+				    wp_nav_menu( $defaults );
+				
+				?>
+				
+<!--				
 				<ul class="main-nav">
 					<li><a href="#">ice cream</a></li>
 					<li><a href="#">donuts</a></li>
 					<li><a href="#">tea</a></li>
 					<li><a href="#">coffee</a></li>
 				</ul>
-			</div>
+-->				
+			</div><!-- ends container one -->
+			
 		</header>
